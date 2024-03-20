4df07a4ee4a9e662aac7f3850d915beb4fee1b3c
rikashi-shifu <harryliow229@gmail.com>
2024-03-20 23:28:31 +0800

    Feat: Added learn.html

---
+++ scripts/learn.js
@@ -0,0 +1,21 @@
+function timelineCardsMouseOverAnimation(current_card, current_position) {
+  current_card.style.transitionDuration = "0.5s";
+  current_card.style.top = `${-2 + current_position}%`;
+}
+
+function timelineCardsMouseLeaveAnimation(current_card, current_position) {
+  current_card.style.transitionDuration = "0.5s";
+  current_card.style.top = `${current_position}%`;
+}
+
+function polaroidCardsMouseOverAnimation(current_card) {
+  let img = current_card.querySelector("img");
+  img.style.transitionDuration = "0.5s";
+  img.style.height = "330px";
+}
+
+function polaroidCardsMouseLeaveAnimation(current_card) {
+  let img = current_card.querySelector("img");
+  img.style.transitionDuration = "0.5s";
+  img.style.height = "300px";
+}
