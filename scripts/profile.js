2b1b643f9dfe2df458d91a852492e07173c02fee
rikashi-shifu <harryliow229@gmail.com>
2024-04-04 15:25:46 +0800

    Feat: Complete validation for all inputs in profile

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,30 +1,41 @@
 function editProfile(btn) {
-    const list_of_inputs = document.querySelectorAll("#user-data .form-control")
-    for (let i = 0; i < list_of_inputs.length; i++) {
-        if (list_of_inputs[i].getAttribute("type") != "email") {
-            list_of_inputs[i].removeAttribute("disabled");
-            list_of_inputs[i].classList.add("border-dark-subtle");
-            list_of_inputs[i].classList.remove("border-0");
-        }
+  const list_of_inputs = document.querySelectorAll("#user-data .form-control");
+  for (let i = 0; i < list_of_inputs.length; i++) {
+    if (list_of_inputs[i].getAttribute("type") != "email") {
+      list_of_inputs[i].removeAttribute("disabled");
+      list_of_inputs[i].classList.add("border-dark-subtle");
+      list_of_inputs[i].className = list_of_inputs[i].className.replace(
+        "border-0",
+        "border-2"
+      );
     }
+  }
 
-    btn.classList.replace("btn-dark", "btn-success");
-    btn.innerText = "Save changes";
-    setTimeout(() => {
-        btn.setAttribute("type", "submit");
-    }, 1);
+  btn.classList.replace("btn-dark", "btn-success");
+  btn.innerText = "Save changes";
+  setTimeout(() => {
+    btn.setAttribute("type", "submit");
+  }, 1);
 }
 
-function validateName(name) {
-    const btn = document.getElementById("save-changes-btn");
-    if (btn.getAttribute("type") == "submit") {        
-        const regex = /^[A-Za-z\s]+$/;
-        if (!regex.test(name.value.trim())) {
-            btn.setAttribute("disabled", "true");
-            name.classList.add("is-invalid");
-        } else {
-            btn.removeAttribute("disabled");
-            name.classList.remove("is-invalid");
-        }
+const btn = document.getElementById("save-changes-btn");
+
+function validateInput(input, regex) {
+  if (btn.getAttribute("type") === "submit") {
+    const isValid = regex.test(input.value.trim());
+    input.classList.toggle("is-invalid", !isValid);
+    setBtnState();
+  }
+}
+
+function setBtnState() {
+  const inputs = document.querySelectorAll("#user-data .form-control");
+  let all_valid = true;
+  for (i = 0; i < inputs.length; i++) {
+    if (inputs[i].className.includes("is-invalid")) {
+      all_valid = false;
+      break;
     }
+  }
+  btn.disabled = !all_valid;
 }
